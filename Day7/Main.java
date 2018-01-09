f066f96e02163cfa4321986797a09d9e854b4b47
LuisCasas <sc.luis@gmail.com>
2018-01-09 23:29:00 +0000

    Day 7 packages and intro to UI

---
+++ Day7/Main.java
@@ -0,0 +1,12 @@
+package com.example.awtSample;
+
+public class Main {
+
+	public static void main(String[] args) {
+		
+		MyWindow window = new MyWindow("Demo window");
+		window.setVisible(true);
+
+	}
+
+}
