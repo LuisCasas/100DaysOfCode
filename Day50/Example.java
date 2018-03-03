eb4af83ec4c85beadc71a3f860dd4252ac39ddf4
LuisCasas <sc.luis@gmail.com>
2018-03-03 23:16:13 +0000

    Day 50: Finish Exceptions and start with Input/Output

--- Day50/Example.java
+++ Day50/Example.java
@@ -7,29 +7,29 @@ import java.util.Scanner;
 public class Example {
 
 	public static void main(String[] args) {
-		
-		int result = divide();
-		System.out.println(result);
+		try {
+			int result = divide();
+			System.out.println(result);
+		} catch(ArithmeticException | NoSuchElementException e) {
+			System.out.println(e.toString());
+			System.out.println("unable to perform division");
+		}
 	}
 	
 	private static int divide() {
 		
 		int x, y;
 		
-		try {
-			x = getInt();
-			y = getInt();
-		} catch(NoSuchElementException e) {
-			throw new ArithmeticException("no suitable input!");
-		}
-		
+//		try {
+		x = getInt();
+		y = getInt();
 		System.out.println("Value of x: " + x + " Value of y: " + y);
-		
-		try {
-			return x/y;
-		} catch(NoSuchElementException e) {
-			throw new ArithmeticException("unable to divide by 0");
-		}
+		return x/y;
+//		} catch(NoSuchElementException e) {
+//			throw new NoSuchElementException("no suitable input!");
+//		} catch(ArithmeticException e) {
+//			throw new ArithmeticException("unable to divide by zero");
+//		}
 	}
 	
 	private static int getInt() {
