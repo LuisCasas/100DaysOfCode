ea5244ca2bd1b0f65d9567e7178ffbb7108857e4
LuisCasas <sc.luis@gmail.com>
2018-03-11 15:27:01 +0000

    Day 57 Start working with Java NIO

--- Day58/Locations.java
+++ Day58/Locations.java
@@ -1,136 +1,72 @@
 package com.luiscasas;
 
+import java.nio.file.*;
 import java.io.*;
 import java.util.*;
 
 public class Locations implements Map<Integer, Location>{
 
 	private static Map<Integer, Location> locations = new LinkedHashMap<>();
-	private static Map<Integer, IndexRecord> index = new LinkedHashMap<>();
-	private static RandomAccessFile ra;
 	
 	public static void main(String[] args) throws IOException {
 	
-		try (RandomAccessFile rao = new RandomAccessFile("locations_rand.dat","rwd")){		
-			rao.writeInt(locations.size());
-			int indexSize = locations.size() * 3 * Integer.BYTES;
-			int locationStart = (int) (indexSize + rao.getFilePointer() + Integer.BYTES);
-			rao.writeInt(locationStart);
-			
-			long indexStart = rao.getFilePointer();
-			
-			int startPointer = locationStart;
-			rao.seek(startPointer);
+		Path locPath = FileSystems.getDefault().getPath("locations_big.txt");
+		Path dirPath = FileSystems.getDefault().getPath("directions_big.txt");
+		
+		try(BufferedWriter locFile = Files.newBufferedWriter(locPath);
+				BufferedWriter dirFile = Files.newBufferedWriter(dirPath)){
 			
 			for(Location location : locations.values()) {
-				rao.writeInt(location.getLocationID());
-				rao.writeUTF(location.getDescription());
-				
-				StringBuilder builder = new StringBuilder();
+				locFile.write(location.getLocationID() + "," + location.getDescription() + "\n");
 				for(String direction : location.getExits().keySet()) {
 					if(!direction.equalsIgnoreCase("Q")) {
-						builder.append(direction);
-						builder.append(",");
-						builder.append(location.getExits().get(direction));
-						builder.append(",");
+						dirFile.write(location.getLocationID() + "," + direction + "," + location.getExits().get(direction) + "\n");
 					}
 				}
-				
-				rao.writeUTF(builder.toString());
-				
-				IndexRecord record = new IndexRecord(startPointer, (int) (rao.getFilePointer() - startPointer));
-				index.put(location.getLocationID(), record);
-				
-				startPointer = (int) rao.getFilePointer();
 			}
-			
-			rao.seek(indexStart);
-			
-			for(Integer locationID : index.keySet()) {
-				rao.writeInt(locationID);
-				rao.writeInt(index.get(locationID).getStartByte());
-				rao.writeInt(index.get(locationID).getLength());
-			}
+		}  catch (IOException io) {
+			System.out.println("IOException:" + io.getMessage());
 		}
-	}
-	
-	static {
-		try {
-			ra = new RandomAccessFile("locations_rand.dat", "rwd");
-			int numLocations = ra.readInt();
-			long locationStartPoint = ra.readInt();
-			
-			while(ra.getFilePointer() < locationStartPoint) {
-				int locationID = ra.readInt();
-				int locationStart = ra.readInt();
-				int locationLength = ra.readInt();
-				
-				IndexRecord record = new IndexRecord(locationStart, locationLength);
-				index.put(locationID, record);
-			}
-			
-		} catch(IOException io) {
-			System.out.println("IOException: " + io.getMessage());
-		}
-	}
-	
-	public Location getLocation(int locationID) throws IOException{
-		
-		IndexRecord record = index.get(locationID);
-		
-		System.out.println(record.getStartByte());
-		
-		ra.seek(record.getStartByte());
-		int id = ra.readInt();
-		String description = ra.readUTF();
-		String exits = ra.readUTF();
-		String[] exitPart = exits.split(",");
-		
-		Location location = new Location(locationID, description, null);
-		
-		if(locationID != 0) {
-			for(int i = 0; i < exitPart.length; i++) {
-				System.out.println("exit part: " + exitPart[i]);
-				System.out.println("exit part [+1]: " + exitPart[i + 1]);
-				
-				String direction = exitPart[i];
-				int destination = Integer.parseInt(exitPart[++i]);
-				location.addExit(direction, destination);
-			}
-		}
-		
-		return location;
 	}
 
-/*
-	static {
+	static {	
+
+		Path locPath = FileSystems.getDefault().getPath("locations_big.txt");
+		Path dirPath = FileSystems.getDefault().getPath("directions_big.txt");
 		
-		try (ObjectInputStream locFile = new ObjectInputStream(new BufferedInputStream(new FileInputStream("locations.dat")))){
+		try(Scanner scanner = new Scanner(Files.newBufferedReader(locPath))){
 			
-			boolean endFile = false;
-			
-			while(!endFile) {
-				
-				try {
-					Location location = (Location) locFile.readObject();
-					
-					System.out.println("Read location: " + location.getLocationID() + " : " + location.getDescription());
-					System.out.println("Found " + location.getExits().size() + " exits");
-					
-					locations.put(location.getLocationID(), location);
-				} catch (EOFException e) {
-					endFile = true;
-				}
+			scanner.useDelimiter(",");
+			while(scanner.hasNextLine()) {
+				int loc = scanner.nextInt();
+				scanner.skip(scanner.delimiter());
+				String description = scanner.nextLine();
+				System.out.println("Imported loc" + loc + ": " + description);
+				locations.put(loc, new Location(loc, description, null));
 			}
-		} catch (InvalidClassException e) {
-			System.out.println("InvalidClassException:" + e.getMessage());
-		} catch (IOException io) {
-			System.out.println("IOException:" + io.getMessage());
-		} catch (ClassNotFoundException e) {
-			System.out.println("ClassNotFound" + e.getMessage());
+		} catch (IOException e) {
+			e.printStackTrace();
 		} 
+		
+		try(BufferedReader dirFile = Files.newBufferedReader(dirPath)){
+			String input;
+			
+			while((input = dirFile.readLine()) != null) {
+				String[] data = input.split(",");
+				int loc = Integer.parseInt(data[0]);
+				String direction = data[1];
+				int destination = Integer.parseInt(data[2]);
+				System.out.println( loc + ": " + direction + ": " + destination);
+				Location location = locations.get(loc);
+				location.addExit(direction, destination);
+				
+			}
+		} catch (IOException e) {
+			e.printStackTrace();
+		} 
+							
 	}
-*/	
+	
 	@Override
 	public void clear() {
 		locations.clear();
@@ -189,10 +125,6 @@ public class Locations implements Map<Integer, Location>{
 	@Override
 	public Collection<Location> values() {
 		return locations.values();
-	}
-	
-	public void close() throws IOException{
-		ra.close();
 	}
 	
 }
--- Day58/Main.java
+++ Day58/Main.java
@@ -1,6 +1,5 @@
 package com.luiscasas;
 
-import java.io.IOException;
 import java.util.HashMap;
 import java.util.Map;
 import java.util.Scanner;
@@ -8,9 +7,8 @@ import java.util.Scanner;
 public class Main {
 	
 	private static Locations locations = new Locations();
-	private static final int startLocation = 1;
 	
-	public static void main(String[] args) throws IOException{
+	public static void main(String[] args) {
 		Scanner scanner = new Scanner(System.in);
 		
 		Map<String, String> vocabulary = new HashMap<String, String>();
@@ -20,16 +18,14 @@ public class Main {
 		vocabulary.put("WEST", "W");
 		vocabulary.put("EAST", "E");
 		
-		Location currentLocation = locations.getLocation(startLocation);
-		
+		int loc = 64;
 		while(true) {
-			System.out.println(currentLocation.getDescription());
-			
-			if(currentLocation.getLocationID() == 0) {
+			System.out.println(locations.get(loc).getDescription());
+			if(loc == 0) {
 				break;
 			}
 			
-			Map<String, Integer> exits = currentLocation.getExits();
+			Map<String, Integer> exits = locations.get(loc).getExits();
 			System.out.println("Available exits:");
 			
 			for(String exit : exits.keySet()) {
@@ -51,12 +47,11 @@ public class Main {
 			}
 			
 			if(exits.containsKey(direction)) {
-				currentLocation = locations.getLocation(currentLocation.getExits().get(direction));
+				loc = exits.get(direction);
 			} else{
 				System.out.println("You cannot go in that direction");
 			}
+			
 		}
-		
-		locations.close();
 	}		
 }
